e59aef5ef1d1d64a611fde6b7f6d99544b468abb
Brayan Izq <brizq02@gmail.com>
2020-12-25 23:19:47 -0500

    Core Assets
    added slip on ramps greater than and equal to 45 degrees

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -14,7 +15,16 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float speed = 5;
     [SerializeField]
-    private float gravity = 150f;
+    private float gravity = 9.8f;
+    private float fallVelocity;
+
+    [SerializeField]
+    private bool isOnSlope = false;
+    private Vector3 hitNormal;
+    [SerializeField]
+    private float slideVelocity;
+    [SerializeField]
+    private float slopeForceDown;
 
     private Vector3 PlayerInput;
     private Vector3 PlayerView;
@@ -30,14 +40,47 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+
         setGravity();
+        slideDown();
         movement();
+        
     }
 
+    private void slideDown()
+     {
+         isOnSlope = Vector3.Angle(Vector3.up, hitNormal) >= player.slopeLimit;
+
+         if (isOnSlope)
+         {
+             PlayerView.x += ((1f - hitNormal.y) * hitNormal.x) * slideVelocity;
+             PlayerView.z += ((1f - hitNormal.z) * hitNormal.z) * slideVelocity;
+
+             PlayerView.y += slopeForceDown;
+         }
+   }
+
+     private void OnControllerColliderHit(ControllerColliderHit hit)
+     {
+         hitNormal = hit.normal;
+     }
+
     private void setGravity()
-    {
-        PlayerView.y = -gravity * Time.deltaTime;
-    }
+     {
+         if (player.isGrounded)
+         {
+             fallVelocity = -gravity * Time.deltaTime;
+             PlayerView.y = fallVelocity;
+         }
+
+         else
+         {
+             fallVelocity -= gravity * Time.deltaTime;
+             PlayerView.y = fallVelocity;
+         }
+
+     }
+     
 
     private void movement()
     {
@@ -47,6 +90,7 @@ public class PlayerController : MonoBehaviour
 
         cameraRotation.CamDirection();
         PlayerView = PlayerInput.x * cameraRotation.camRigth + PlayerInput.z * cameraRotation.camForward ;
+        PlayerView = PlayerView * speed;
         player.transform.LookAt(player.transform.position + PlayerView);
     }
 
