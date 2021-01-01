5b601830bf28c14bf22047b7b1012df029cb1677
Brayan Izq <brizq02@gmail.com>
2021-01-01 13:00:05 -0500

    Core Assets
    Fix Bugs

--- Assets/Scripts/Joystick.cs
+++ Assets/Scripts/Joystick.cs
@@ -1,17 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class Joystick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
+//Hecho por RickRock666: ... 29/12/2020...
+//siganlo en su canal :D
+
+public class Joystick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
 
     public RectTransform handle;
     public RectTransform outerHandle;
 
     private Vector2 pointA;
-    private Vector2 drag;
+    public Vector2 drag;
     [SerializeField]
     private Vector2 axis;
     public Vector2 Axis { get { return axis; } }
@@ -19,34 +20,39 @@ public class Joystick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPoint
     public float radioOffset = 10;
     private float x;
     private float y;
-
+    //la base donde se detectan los clicks
     private Image baseClickHandler;
     private Vector2 tempPosition;
-    private bool onTouch;
+    public bool onTouch = false;
+    private int id;
 
-    //la base donde se detectan los clicks
     void Start()
     {
         baseClickHandler = baseClickHandler ? GetComponent<Image>() : gameObject.AddComponent(typeof(Image)) as Image;
         baseClickHandler.color = new Color(0, 0, 0, 0);
         tempPosition = outerHandle.position;
+        handle.sizeDelta = outerHandle.sizeDelta / 2;
+        baseClickHandler.rectTransform.offsetMax = new Vector2(-640, 0);
     }
+
     //el joystick toma punto de referencia
     public void OnPointerDown(PointerEventData eventData)
     {
-        if (eventData.position.x < Screen.width / 2)
+        if (!onTouch)
         {
+            id = eventData.pointerId;
             pointA = eventData.position;
-            handle.sizeDelta = outerHandle.sizeDelta / 2;
             outerHandle.transform.position = pointA;
             onTouch = true;
+            baseClickHandler.enabled = false;
         }
     }
-
     //movemos con sus limitaciones al joystick
     public void OnDrag(PointerEventData eventData)
     {
-        if (eventData.position.x < Screen.width / 2 && onTouch)
+        Vector2 local = Vector2.zero;
+        if (onTouch && RectTransformUtility.ScreenPointToLocalPointInRectangle
+            (handle, eventData.position, eventData.pressEventCamera, out local) && eventData.pointerId == id)
         {
             drag = eventData.position;
             Vector2 offset = drag - pointA;
@@ -65,19 +71,23 @@ public class Joystick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPoint
             axis = new Vector2(x, y);
             handle.transform.position = new Vector2(pointA.x + direccion.x, pointA.y + direccion.y);
         }
-    }
 
+    }
 
     //reseteamos todo al inicio
     public void OnPointerUp(PointerEventData eventData)
     {
-        if (eventData.position.x < Screen.width / 2)
+        if (onTouch && eventData.pointerId == id)
         {
             axis = Vector2.zero;
             handle.transform.localPosition = Vector2.zero;
             outerHandle.position = tempPosition;
             onTouch = false;
+            drag = Vector2.zero;
+            baseClickHandler.enabled = true;
         }
 
     }
+
+
 }
